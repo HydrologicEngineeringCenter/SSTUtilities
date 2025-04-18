35c69937b908e231ba4c7cd7aa05174615b287ff
Greg Karlovits <gregory.s.karlovits@usace.army.mil>
2025-04-18 14:21:56 -0500

    added flow extraction functions, added element name to csv output

--- HMSPostProcessor.py
+++ HMSPostProcessor.py
@@ -25,7 +25,23 @@ def extract_annual_maximum_precipitation(precip_pd: PairedData, ev_per_year: int
     df_ams = df.groupby('Year')['Precip Total'].max().reset_index()
     return df_ams
 
-def write_ams_to_csv(ams_df: pd.DataFrame, realiz_name: str, project_path: str) -> None:
+def extract_flow_results(realiz_name: str, project_path: str, b_part: str) -> PairedData :
+    dss_file = f"{project_path}/{realiz_name.replace(' ', '_')}.dss"
+    pathname_total_precip = \
+        f"//{b_part}/Realization-Outflow Maximum///MCA:{realiz_name}/"
+    with HecDss(dss_file) as dss:
+        total_precip = dss.get(pathname_total_precip)
+    return total_precip
+
+def extract_annual_maximum_flow(flow_pd: PairedData, ev_per_year: int, num_years: int) -> pd.DataFrame:
+    year_list = list(range(1, num_years + 1))
+    repeated_year_list = [i for i in year_list for _ in range(ev_per_year)]
+    df = pd.concat([pd.Series(repeated_year_list), pd.Series(flow_pd.values.flatten())], axis = 1)
+    df.columns = ['Year', 'Maximum Flow']
+    df_ams = df.groupby('Year')['Maximum Flow'].max().reset_index()
+    return df_ams
+
+def write_ams_to_csv(ams_df: pd.DataFrame, realiz_name: str, b_part: str, project_path: str) -> None:
     san_mca_name = realiz_name.replace(" ", "_")
-    op_fname = f"{project_path}/data/{san_mca_name}_ams.csv"
+    op_fname = f"{project_path}/data/{san_mca_name}_{b_part}_ams.csv"
     ams_df.to_csv(op_fname, index = False)
