d50b0efdc5ce7c0c748d2fdb2ce014cbc90cbc55
ejgumbel <g.karlovits@gmail.com>
2025-07-22 09:42:29 -0500

    Created the gage stuffer utility

---
+++ GageStuffer.py
@@ -0,0 +1,57 @@
+from hecdss import HecDss, DssPath
+import datetime
+
+modified_datetime = datetime.datetime.now()
+
+class HmsFlowGageGenerator:
+    def __init__(self, dsspath: DssPath, filename: str):
+        self.path = dsspath
+        self.filename = filename
+
+    def gageName(self) -> str:
+        timestep = "D" if self.path.E == "1Day" else "Inst"
+        return self.path.A + " " + self.path.B + " " + timestep
+
+    def produceLines(self) -> list[str]:
+        lines = ["\n",
+                 f"Gage: {self.gageName()}\n",
+                 f"     Gage: {self.gageName()}\n",
+                 "     Gage Type: Flow\n",
+                 f"     Last Modified Date: {modified_datetime.strftime("%#d %B %Y")}\n",
+                 f"     Last Modified Time: {modified_datetime.strftime('%H:%M:%S')}\n",
+                 "     Reference Height Units: Feet\n",
+                 "     Reference Height: 32.80800\n",
+                 "     Reference Height Units: Feet\n",
+                 "     Station Id:\n",
+                 "     Data Source Type: External DSS\n",
+                 f"     Filename: {self.filename}\n",
+                 f"     Pathname: {self.path}\n",
+                 "     Variant: Variant-1\n",
+                 "       Start Time: 1 January 2000, 00:00\n",
+                 "       End Time: 2 January 2000, 00:00\n",
+                 "     End Variant: Variant-1\n", "End:\n"]
+        return lines
+
+gage_data_file = "C:/data/iowa_river_usgs_timeseries.dss"
+gage_dss_file = HecDss(gage_data_file)
+
+output_file_name = "C:/data/iowa_river_usgs_timeseries_flow.gage"
+
+dss_cat = gage_dss_file.get_catalog()
+
+dss_cat_items = dss_cat.items
+
+master_line_list = []
+for item in dss_cat_items:
+    if item.C == "FLOW":
+        this_generator = HmsFlowGageGenerator(item, gage_data_file)
+        master_line_list.append(this_generator.produceLines())
+
+gage_dss_file.close()
+
+flat_line_list = [x for xs in master_line_list for x in xs]
+
+with open(output_file_name, "w") as o:
+    for line in flat_line_list:
+        o.write(line)
+
