95a7288e48f7cf3b173750aa4eda52c3c47964ac
Greg Karlovits <gregory.s.karlovits@usace.army.mil>
2025-10-09 12:46:10 -0500

    Wrapped basin model and control spec processing loop in a function and modified Trinity implementation to use the new function call.

--- TrinityFFRDHotStarter.py
+++ TrinityFFRDHotStarter.py
@@ -1,27 +1,16 @@
-from hecdss import HecDss
-from HotStartBasinFile import generate_hotstarted_basin_file
-from ControlSpecificationGenerator import generate_control_spec
+from datetime import datetime
 
-from datetime import datetime, timedelta
+from HotStartProcessingFunction import process_basin_files
 
 filepath = "C:/Users/q0hecgsk/Documents/Models/Daily_POR_data-trinity"
 basin_models = ["trinity_apr_may_1990", "trinity_aug_sep_2017"]
 por_run_name = "por"
 states_start_date = datetime.strptime("1979-10-01", '%Y-%m-%d')
 states_end_date = datetime.strptime("1981-10-01", '%Y-%m-%d')
-state_dates = [states_start_date + timedelta(days = t) for t in range((states_end_date - states_start_date).days + 1)]
-
 event_duration_hours = 72
 lookback = 0
 padding = 14 * 60 - event_duration_hours
-base_control_spec = f"{filepath}/POR_1979_2022.control"
+base_control_spec = "POR_1979_2022"
 
-por_filename = f"{filepath}/{por_run_name.replace(' ', '_')}.dss"
-por_dss = HecDss(por_filename)
-
-for date in state_dates:
-    generate_control_spec(date, event_duration_hours, lookback, padding, base_control_spec)
-    for base in basin_models:
-        generate_hotstarted_basin_file(filepath, base, por_run_name, por_dss, date)
-
-por_dss.close()
+process_basin_files(filepath, basin_models, por_run_name, states_start_date, states_end_date,
+                    event_duration_hours, lookback, padding, base_control_spec)
